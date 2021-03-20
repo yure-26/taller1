ceeb24e0ec95c1adf474dfd38438ec640459c150
Dayana Silva <ydsb2000@gmail.com>
2021-03-19 22:10:00 -0500

    Taller 1Para entregrar

---
+++ index.php
@@ -0,0 +1,61 @@
+
+	 public static void vect(int [] A){
+		 int i, j, aux;
+		 boolean llenar=true;
+		 for(i=0;i<A.length-1 && llenar;i++)
+		 {
+		 llenar=false;
+		 for(j=0;j<A.length-i-1;j++)
+		 if(A[j+1]<A[j])
+		 {
+		 aux=A[j+1];
+		 A[j+1]=A[j];
+		 A[j]=aux;
+		 llenar =true;
+		    }
+	    }
+     }
+  }
+  <html>
+    <head>
+        </title>Taller 1 </title>
+    </head>
+    <body>
+    <?php
+        $varname = "yure";
+        $varedad  = 20;
+        $varb    = true;
+        $varh    = 1.63;
+        
+    ?>
+        <h1>
+            <?php
+                echo  $varname;
+            ?>
+        </h1>
+
+        <h2>
+            <?php
+                echo $varedad;
+            ?>
+        </h2>
+
+        <?php
+        $posiciones=[
+            "Asus",
+            "Gygabyte",
+            "NXzt",
+            "Intel",
+            "AMD"
+        ];
+
+        $componetes_PC = count($posiciones);
+        echo "Mi vector tiene".$componetes_PC;
+
+        for ($i=0; $i < $componetes_PC; $i++) 
+        { 
+            echo $posiciones[ $i];
+        }
+        ?>
+    
+    </body>
